79febdef0df217ed5cca4db3580a919011ef607e
Dmitry <todem5@gmail.com>
2024-12-26 12:45:10 +0600

    first commit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\tarifsname;
 
 class SiteController extends Controller
 {
@@ -61,7 +62,14 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        
+        $tarifsname = TarifsName::find()->orderBy('name')->all();
+
+
+        return $this->render('index', 
+        [
+            'model' => $tarifsname,
+        ]);
     }
 
     /**
